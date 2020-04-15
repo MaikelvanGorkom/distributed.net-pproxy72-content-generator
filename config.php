4ea53ca066b67b8f99504f35a555f9186ca22ca2
mvgorkom <mvgorkom@SRV-VM-WEB-01.PROJECT34.network>
2020-04-16 00:38:45 +0200

    Updated to include a config file, so you don't have to edit the main file.

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+$START_FROM_DATE = "13 April 2017";
+$END_ON_DATE = "13 April 2020";
+$PROXYFILE = "pproxyrc572";
+?>
--- gen.php
+++ gen.php
@@ -1,4 +1,11 @@
 <?php
+
+include "config.php";
+$EMAILFILE = "email.txt";
+$OSFILE = "os.txt";
+$CPUFILE = "cpu.txt";
+$CLIENTVERSIONFILE = "clientversion.txt";
+
 function LOAD_FILE_TO_ARRAY($FILE){
     $ARRAY = file($FILE);
     return $ARRAY;
@@ -15,25 +22,17 @@ function RANDOM_STRING($LN){
 }
 
 $NEEDED_ROWS = 100000;
-$STARTDATE = strtotime("13 april 2017");
-$ENDDATE = strtotime("13 April 2020");
+$STARTDATE = strtotime($START_FROM_DATE);
+$ENDDATE = strtotime($END_ON_DATE);
 
-$OUTPUTFILE = 'pproxyrc572';
+$OUTPUTFILE = $PROXYFILE;
 $TMPFILE = $OUTPUTFILE."tmp";
 
-$EMAILS = LOAD_FILE_TO_ARRAY("email.txt");
-$OS = LOAD_FILE_TO_ARRAY("os.txt");
-$CPU = LOAD_FILE_TO_ARRAY("cpu.txt");
-$CV = LOAD_FILE_TO_ARRAY("clientversion.txt");
+$EMAILS = LOAD_FILE_TO_ARRAY($EMAILFILE);
+$OS = LOAD_FILE_TO_ARRAY($OSFILE);
+$CPU = LOAD_FILE_TO_ARRAY($CPUFILE);
+$CV = LOAD_FILE_TO_ARRAY($CLIENTVERSIONFILE);
 $DNETCLINE ='';
-//2019-04-12 19:13:10,192.168.178.1,mail@project34.net,D9:BCB5714A:00000000,64,1,19,91100519,0
-//2019-04-12 19:20:20,192.168.178.1,mail@project34.net,D9:BCB5718A:00000000,64,1,19,91100519,0
-
-//2012-10-24 15:32:57,192.168.178.218,U@somewhere.network,1X:96LBCBUH:00000000,21,33,8,0,0
-//2016-07-18 22:35:04,192.168.178.79,F@somewhere.network,D0:ZZ6IA4NM:00000000,6,19,16,3,0
-
-
-
 
 for ($i=1; $i<= $NEEDED_ROWS;$i++){
 	$DATE_TO_USE = mt_rand($STARTDATE, $ENDDATE);
